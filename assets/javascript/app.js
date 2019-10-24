252b25ee69f54b50ad8f6221b0d79cf49021356a
Andre Morrall <andre.morrall@corgan.com>
2019-10-24 16:37:26 -0500

    Answers populate to HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-/* $(document).ready(function () {
+$(document).ready(function () {
     $("#game-timer").parent().hide();
     $(document).on('click', '#start-game', function () {
         $("#start-game").hide();
@@ -13,48 +13,54 @@
             }
             $("#game-timer").text(seconds);
         }, 1000);
-        $("#game-questions").html(gameQuestions[0].question); // Figure out how to get content to show all questions in the HTML
-    }); */
-    
+        $("#game-questions").html(gameQuestions[q]); // Figure out how to get content to show all questions in the HTML
+    });
+
     // Create an array of objects for game questions
-    
-    // Questions
     var gameQuestions = [
-    {
-        question: 'Who framed Roger Rabbit?',
-        options: ['Bob', 'Jeff', 'The Fuzz', 'Peter'],
-        answer: 'The Fuzz',
-    },
-    {
-        question: 'Who framed Roger Bear?',
-        options: ['Bob', 'Jeff', 'The Fuzz', 'Peter'],
-        answer: 'Bob'
-    },
-    {
-        question: 'Who framed Roger Dog?',
-        options: ['Bob', 'Jeff', 'The Fuzz', 'Peter'],
-        answer: 'Jeff',
-    },
-    {
-        question: 'Who framed Roger Cat?',
-        options: ['Bob', 'Jeff', 'The Fuzz', 'Peter'],
-        answer: 'The Fuzz',
-    },
-    {
-        question: 'Who framed Roger Tiger?',
-        options: ['Bob', 'Jeff', 'The Fuzz', 'Peter'],
-        answer: 'Peter',
-    },
-];
+        {
+            question: 'Who framed Roger Rabbit?',
+            options: ['Bob', 'Jeff', 'The Fuzz', 'Peter'],
+            answer: 'The Fuzz',
+        },
+        {
+            question: 'Who framed Roger Bear?',
+            options: ['Bob', 'Jeff', 'The Fuzz', 'Peter'],
+            answer: 'Bob'
+        },
+        {
+            question: 'Who framed Roger Dog?',
+            options: ['Bob', 'Jeff', 'The Fuzz', 'Peter'],
+            answer: 'Jeff',
+        },
+        {
+            question: 'Who framed Roger Cat?',
+            options: ['Bob', 'Jeff', 'The Fuzz', 'Peter'],
+            answer: 'The Fuzz',
+        },
+        {
+            question: 'Who framed Roger Tiger?',
+            options: ['Bob', 'Jeff', 'The Fuzz', 'Peter'],
+            answer: 'Peter',
+        },
+    ];
 
     // Testing code for game questions
-/*     function showQuestions() {
-        $("#game-questions").text(gameQuestions[i].question) */
+
+        // Code to show questions.
         for (var q = 0; q < gameQuestions.length; q++) {
-            $("#game-questions").append("<h4>" + gameQuestions[q].question + "</h4>")
-            console.log(gameQuestions[q]);
+        $("#game-questions").append("<p>" + gameQuestions[q].question + "</p>")
+        console.log(gameQuestions[q]);
+        
+        // Code to show answers. Create buttons
+        for (var a = 0; a < gameQuestions.length; a++) {
+        //Testing layout of answers to show on HTML
+            $("#game-questions").append("<p>" + gameQuestions[a].options + "</p>");
         }
-    
+    }
+
+    // Code by tutor
     setTimeout(() => {
         console.log($('input:checked'))
     }, 3000)
+})
